34270e83cbd578e2d0eac8c514b006c4e26f0501
Osman Baskaya <thyme.exe@gmail.com>
2019-09-11 22:01:05 -0700

    some improvements.

--- neural_wsd/text/dataset.py
+++ neural_wsd/text/dataset.py
@@ -1,15 +1,44 @@
 import os
+import random
 
+import math
 import pandas as pd
 import torch.utils.data
+from torch.utils.data.sampler import SubsetRandomSampler, SequentialSampler
+
+SEED = 42
+
+
+def sample_data(dataset, size_of_first_dataset=0.8, shuffle=True, seed=SEED):
+    """Split the `dataset` into two and returns a sampler for each split"""
+
+    random.seed(seed)
+
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+
+    first_dataset_size = int(math.ceil(size_of_first_dataset * dataset_size))
+
+    if shuffle:
+        random.shuffle(indices)
+
+    first_data_indices = indices[:first_dataset_size]
+    second_data_indices = indices[first_dataset_size:]
+    return SubsetRandomSampler(first_data_indices), SubsetRandomSampler(second_data_indices)
 
 
 class WikiWordSenseDisambiguationDataset(torch.utils.data.Dataset):
-    def __init__(self, directory):
-        self.directory = directory
-        self.__data = WikiWordSenseDisambiguationDataset.read_data_to_dataframe(directory)
-        self._labels = self.data["sense"].unique()
+    def __init__(self, data=None, label_column=None):
+        if label_column is None:
+            label_column = "sense"
+        self.__data = data
+        self._labels = self.data[label_column].unique()
         self._num_of_unique_labels = len(self._labels)
+
+    @staticmethod
+    def from_tsv(directory, label_column=None):
+        data = WikiWordSenseDisambiguationDataset.read_data_to_dataframe(directory)
+        return WikiWordSenseDisambiguationDataset(data=data, label_column=label_column)
 
     @property
     def data(self):
@@ -25,6 +54,7 @@ class WikiWordSenseDisambiguationDataset(torch.utils.data.Dataset):
 
     @staticmethod
     def read_data_to_dataframe(directory, column_names=None):
+        # TODO: replace here with a general solution
         if column_names is None:
             column_names = [
                 "id",
@@ -50,6 +80,7 @@ class WikiWordSenseDisambiguationDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         row = self.data.iloc[idx]
+        # Todo: replace here with dict(**row).
         sample = {
             "text": row["tokenized_sentence"],
             "label": row["sense"],
